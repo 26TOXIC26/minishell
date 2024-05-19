f9bbc4e4d4de428a5e388fdd4ca58d5e2b330ff4
bamssaye <bamssaye@e2r6p12.1337.ma>
2024-05-20 00:29:29 +0100

    update

---
+++ include/minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bamssaye <bamssaye@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 17:20:03 by bamssaye          #+#    #+#             */
+/*   Updated: 2024/05/08 17:25:02 by bamssaye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>      
+#include <signal.h>     
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>    
+#include <errno.h>    
+#include <termios.h>
+#include <curses.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+# endif
