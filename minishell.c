d4e5456f94c15b2253812ff3f5b9292326b75bdd
Abdelilah Mousaid <51842546+26TOXIC26@users.noreply.github.com>
2024-06-02 11:13:14 +0000

    start pars

--- minishell.c
+++ minishell.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abdelilah <abdelilah@student.42.fr>        +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:20:08 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/05/30 06:23:14 by abdelilah        ###   ########.fr       */
+/*   Updated: 2024/06/02 11:01:47 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
-#include <stdio.h>
 
 
 
@@ -126,6 +125,97 @@ void edit_pwd(size_t i, t_minishell *mini)
     }
 }
 
+int check_quote(t_minishell mini)
+{
+    int i;
+    char q;
+
+    i = 0;
+    while (mini.line[i])
+    {
+        if (mini.line[i] == '\'' || mini.line[i] == '\"')
+        {
+            q = mini.line[i];
+            i++;
+            while (mini.line[i] != q && mini.line[i])
+                i++;
+            if (mini.line[i] != q)
+            {
+                printf("quote not closed\n");
+                return (0);
+            }
+        }
+        i++;
+    }
+    return (1);
+}
+
+int    is_space(char *s)
+{
+    while ((*s >= 9 && *s <= 13) || *s == 32)
+        s++;
+    if (*s == '\0')
+         return (0);
+    return (1);    
+}
+
+int check_syntax2(t_minishell mini)
+{
+    int i;
+
+    i = 0;
+    while (mini.line[i])
+    {
+        if (mini.line[i] == '|' && (is_space(mini.line + i + 1) == 0 || mini.line[i + 1] == '|'))
+            return (printf("syntax error near unexpected token `||' or 'newline'\n") && 0);
+        else if (mini.line[i] == '&' || mini.line[i] == ';')
+            return (printf("syntax error near unexpected token `%c'\n", mini.line[i]) && 0);
+        else if (mini.line[i] == '<' && mini.line[i + 1] == '>')
+            return (printf("syntax error near unexpected token `<>'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '<')
+            return (printf("syntax error near unexpected token `><'\n") && 0);
+        else if (mini.line[i] == '<' && mini.line[i + 1] == '<' && mini.line[i + 2] == '<')
+            return (printf("syntax error near unexpected token `<<<'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '>' && mini.line[i + 2] == '>')
+            return (printf("syntax error near unexpected token `>>>'\n") && 0);
+        else if (mini.line[i] == '<' && mini.line[i + 1] == '<' && mini.line[i + 2] == '>')
+            return (printf("syntax error near unexpected token `<<<>'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '>' && mini.line[i + 2] == '<')
+            return (printf("syntax error near unexpected token `>><'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '>' && mini.line[i + 2] == '>')
+            return (printf("syntax error near unexpected token `>>>'\n") && 0);
+        else if (mini.line[i] == '<' && mini.line[i + 1] == '<' && mini.line[i + 2] == '<')
+            return (printf("syntax error near unexpected token `<<<'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '>' && mini.line[i + 2] == '>')
+            return (printf("syntax error near unexpected token `>>>'\n") && 0);
+        else if (mini.line[i] == '<' && mini.line[i + 1] == '<' && mini.line[i + 2] == '>')
+            return (printf("syntax error near unexpected token `<<<>'\n") && 0);
+        else if (mini.line[i] == '>' && mini.line[i + 1] == '>' && mini.line[i + 2] == '<')
+            return (printf("syntax error near unexpected token `>><'\n") && 0);
+        i++;
+    }
+    return (1);
+}
+
+int check_syntax(t_minishell mini)
+{
+    if (!check_quote(mini))
+        return (0);
+    else if (mini.line[0] == '|')
+        return (printf("syntax error near unexpected token `%c'\n", mini.line[0]) && 0);
+    else if ((mini.line[0] == '>' || mini.line[0] == '<') && (mini.line[1] == '\0' || mini.line[1] == '|' || mini.line[1] == ';'))
+        return (printf("syntax error near unexpected token `%c'\n", mini.line[0]) && 0);
+    else if (((mini.line[0] == '>' && mini.line[1] == '>')  || (mini.line[0] == '<' &&  mini.line[1] == '<')) && mini.line[2] == '\0')
+        return (printf("syntax error near unexpected token `>>' OR '<<'\n") && 0);
+    else if ((mini.line[0] == '<' && mini.line[1] == '>') || (mini.line[0] == '>' && mini.line[1] == '<'))
+        return (printf("syntax error near unexpected token `><' OR '<>'\n") && 0);
+    if (check_syntax2(mini) == 0)
+        return (0);
+    return (1);
+}
+
+
+
 int main(int ac, char **av, char **env)
 {
     t_minishell mini;
@@ -140,25 +230,29 @@ int main(int ac, char **av, char **env)
     {        
         
         mini.line = readline(CYAN BOLD "MINIHELL $> "RESET);
-        if (!ft_strncmp(mini.line, "env", ft_strlen("env")))
-            print_env(&mini);
-        else if (!ft_strncmp(mini.line, "pwd", ft_strlen("pwd")))
-            ft_pwd();
-        else if (!ft_strncmp(mini.line, "exit", ft_strlen("exit")))
-            break ;
-        else if (!ft_strncmp(mini.line, "cd", ft_strlen("cd")))
+        add_history(mini.line);
+        if (check_syntax(mini) == 1)
         {
-            edit_old_pwd(find_env("OLDPWD", &mini) ,&mini);
-            if (ft_strlen(mini.line) == 2)
-                ft_cd(getenv("HOME"));
-            else
-                ft_cd(mini.line + 3);
-            edit_pwd(find_env("PWD", &mini), &mini);
+            if (!ft_strncmp(mini.line, "env", ft_strlen("env")))
+                print_env(&mini);
+            else if (!ft_strncmp(mini.line, "pwd", ft_strlen("pwd")))
+                ft_pwd();
+            else if (!ft_strncmp(mini.line, "exit", ft_strlen("exit")))
+                break ;
+            else if (!ft_strncmp(mini.line, "cd", ft_strlen("cd")))
+            {
+                edit_old_pwd(find_env("OLDPWD", &mini) ,&mini);
+                if (ft_strlen(mini.line) == 2)
+                    ft_cd(getenv("HOME"));
+                else
+                    ft_cd(mini.line + 3);
+                edit_pwd(find_env("PWD", &mini), &mini);
+            }
+            // else
+            //     exec_cmd(mini.line);
+            free(mini.line);
+            // mini.line = NULL
         }
-        // else
-        //     exec_cmd(mini.line);
-        free(mini.line);
-        // mini.line = NULL;
     }
     return (0);
 }
