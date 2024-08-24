c1025560f7a25319e3d4be5fee1679304f6d9986
Brahim Amssayef <142234204+bamssaye@users.noreply.github.com>
2024-08-24 02:35:00 +0100

    Update unset.c

--- bultin/unset.c
+++ bultin/unset.c
@@ -6,30 +6,60 @@
 /*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 14:59:32 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/08/24 01:12:55 by bamssaye         ###   ########.fr       */
+/*   Updated: 2024/08/24 02:27:14 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+
 void	env_free(t_env **node)
 {
-	// int	i;
+	t_env 	*tmp;
+	t_env	*cur;
+
 	if (!node || !*node)
 		return ;
-	// i = 0;
-	(*node)->next = NULL;
-	free((*node)->line[0]);
-	free((*node)->line[1]);
-	// free((*node)->line);
-	free(*node);
+	cur = *node;
+	while (cur)
+	{
+		tmp = cur->next;
+		if (cur->line[0])
+		{
+			free(cur->line[0]);
+			cur->line[0] = NULL;
+		}
+		if (cur->line[1])
+		{
+			free(cur->line[1]);
+			cur->line[1] = NULL;
+		}
+		free(cur);
+		cur = tmp;
+	}
 	*node = NULL;
 }
+void delete_node(t_env **env, t_env *delnode, t_env *prev)
+{
+    t_env *node;
+
+    if (!delnode)
+        return;
+    node = delnode->next;
+    if (prev)
+        prev->next = node;
+	else
+        *env = node;
+    delnode->next = NULL;
+    env_free(&delnode);
+}
+
 void	env_delete(t_env **env, char *str)
 {
 	t_env	*tmp;
-	t_env	*node;
+	t_env	*pev;
 
+	pev = NULL;
 	if (!env || !*env || !str || !*str)
 		return ;
 	tmp = *env;
@@ -37,14 +67,14 @@ void	env_delete(t_env **env, char *str)
 	{
 		if (!ft_strncmp(tmp->line[0], str, ft_strlen(str)))
 		{
-			node = tmp;
-			tmp = tmp->next;
-			*env = (*env)->next;
-			node->next = NULL;
-			env_free(&node);
+			delete_node(env, tmp, pev);
+            tmp = *env; 
 		}
 		else
-			tmp = tmp->next;
+		{
+			pev = tmp;
+            tmp = tmp->next;
+		}
 	}
 }
 void	ft_unset(t_command *cmd, t_env **env)
