a47d7af717045816fd85bb442c1867d48f4c739b
Brahim Amssayef <142234204+bamssaye@users.noreply.github.com>
2024-08-25 22:58:12 +0100

    Update export.c

--- bultin/export.c
+++ bultin/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amousaid <amousaid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bamssaye <bamssaye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 14:58:38 by bamssaye          #+#    #+#             */
-/*   Updated: 2024/08/24 21:50:37 by amousaid         ###   ########.fr       */
+/*   Updated: 2024/08/25 22:58:02 by bamssaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,6 @@ void	set_index(char **line, t_env *env)
 		i++;
 	}
 }
-
 void	sort_env(t_env *env)
 {
 	int		i;
@@ -79,21 +78,61 @@ void	sort_env(t_env *env)
 	sort_env_1(line);
 	set_index(line, env);
 	arry_c(line);
+	print_exp(env);
 }
-
-void	ft_export(t_main *cmd, t_command *mini)
+void	export_solo(char *flag, t_main *mini)
 {
-	// if (ft_export2(cmd) == 1)
-	// 	return ;
-	(void)mini;
-	// t_env *sorted_list;
-	sort_env(cmd->env);
-	// sorted_list = merge_sort(cmd->env);
-	print_exp(cmd->env);
-	// print_exp(cmd->env);
-	// while (cmd && cmd->type == 1)
-	// {
-	// 	ft_export_run(cmd, mini);
-	// 	cmd = cmd->next;
-	// }
+	if (ch_exp(flag, ft_strlen(flag)))
+		pexport_e(flag);
+	else
+	{
+		ft_env_back(&mini->env, creat_new_env(flag, 1));
+	}
+}
+int	_checkexp(char *str, int f)
+{
+	int	i;
+
+	if (!str || str[0] == '\0')
+		return (0);
+	i = 0;
+	if (ft_isalpha(str[i]) == 0 && str[i] != '_')
+		return (1);
+	else
+		i++;
+	while (str[i] && i < f)
+	{
+		if (ft_isalnum(str[i]) == 0 && str[i] != '_')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+void	ft_export(t_main *mini, t_command *cmd)
+{
+	int	i;
+	int	equal;
+
+	i = 1;
+	if (!cmd->options[1])
+		sort_env(mini->env);
+	while (cmd->options[i])
+	{
+		if (!ch_eq(cmd->options[i]))
+			export_solo(cmd->options[i], mini);
+		else
+		{
+			equal = eq_pos(cmd->options[i]);
+			if (!equal)
+				pexport_e(cmd->options[i]);
+			else
+			{
+				if (_checkexp(cmd->options[i], equal))
+					pexport_e(cmd->options[i]);
+				else
+					update_env(cmd->options[i], mini);
+			}
+		}
+		i++;
+	}
 }
